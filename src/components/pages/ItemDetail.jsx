42979da1ea3cfdca5cc52699086791bdd1ce600e
namkyungLee <namkyung.lee@futuresense.co.kr>
2022-10-24 18:52:15 +0900

    style 정리

--- src/components/pages/ItemDetail.jsx
+++ src/components/pages/ItemDetail.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Table } from "react-bootstrap";
 import { useNavigate, useParams } from "react-router-dom";
 import styled, { createGlobalStyle } from "styled-components";
